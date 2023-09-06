2630392071b0809aec88203a6fd927fb87bbe4d9
Jean <jeanportugalcadastrar@gmail.com>
2023-09-06 23:51:55 +0100

    created new folder 6 decision block

---
+++ 6-decisonBlock/takeDecision/src/Main.java
@@ -0,0 +1,16 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+
+        int age = 17;
+        boolean adult;
+        if(age >= 18){
+            adult = true;
+            System.out.printf("Ok done. Adult = "+ adult);
+        }else{
+            adult = false;
+            System.out.printf("Almost done. Adult = "+ adult);
+        }
+    }
+}
