12dfeba649fbf87abc7faa74692925e00045aaa0
Jean <jeanportugalcadastrar@gmail.com>
2024-03-03 18:32:27 +0000

    feat(math): added new function calculate x,y

--- 5-mathExpression/src/Application.java
+++ 5-mathExpression/src/Application.java
@@ -2,35 +2,31 @@ import java.util.Scanner;
 
 public class Application {
     public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
 
-        Scanner sc = new Scanner(System.in);
-        int x,a,b,c;
-        double delta,xLine;
+    System.out.println("+--------------------------------------+");
+    System.out.println("|Calculate the expression: ax²+bx+c = 0|");
+    System.out.println("+--------------------------------------+");
 
-        System.out.println("+--------------------------------------+");
-        System.out.println("|Calculate the expression: ax²+bx+c = 0|");
-        System.out.println("+--------------------------------------+");
+    System.out.print("Give a value to a: ");
+    int a = sc.nextInt();
+    System.out.print("\nGive a value to b: ");
+    int b = sc.nextInt();
+    System.out.print("\nGive a value to c: ");
+    int c = sc.nextInt();
+    System.out.println("+--------------------------------------+");
+    System.out.printf("\t exrpression: %dx² + %dx + %d = 0 \n", a, b, c);
+    System.out.println("+--------------------------------------+");
+    System.out.println();
 
-        System.out.print("Give a value to a: ");
-        a = sc.nextInt();
-        System.out.print("\nGive a value to b: ");
-        b = sc.nextInt();
-        System.out.print("\nGive a value to c: ");
-        c = sc.nextInt();
-        System.out.println("+--------------------------------------+");
-        System.out.printf("\t exrpression: %dx² + %dx + %d = 0 \n",a,b,c);
-        System.out.println("+--------------------------------------+");
-        System.out.println();
+    double delta = Math.pow(b, 2) - (4 * a * c);
+    System.out.println("delta = " + delta);
 
-        delta = (Math.pow(b,2)) - (4 * a * c);
-        System.out.println("delta = "+delta);
+    System.out.println("X1 = " + calculateX(a, b, c, delta, 1));
+    System.out.println("X2 = " + calculateX(a, b, c, delta, -1));
+}
 
-        xLine = (-(b) + Math.sqrt(delta))/(2 * a);
-        System.out.println("X1 = "+xLine);
-
-        xLine = (-(b) - Math.sqrt(delta))/(2 * a);
-        System.out.println("X2 = "+xLine);
-
-
-    }
+private static double calculateX(int a, int b, int c, double delta, int sign) {
+    return (-b + sign * Math.sqrt(delta)) / (2 * a);
+}
 }
